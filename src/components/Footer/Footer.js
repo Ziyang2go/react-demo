0db1b81638fdfa9c9acdc7199cbb696dea7ceace
Ziyang <wangziyang507@gmail.com>
2017-04-12 22:38:43 -0400

    change footer style

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,16 +3,16 @@ import { IndexLink, Link } from 'react-router'
 import './Footer.scss'
 
 export const Footer = () => (
-  <div className="ui inverted vertical footer segment">
+  <div className="ui inverted vertical footer segment" style={{height:'50px'}}>
     <div className="ui container">
       <div className="ui inverted stackable grid">
-        <div>
+        <div style={{marginTop:'10px'}} >
           <p>© 2017 make order easy</p>
         </div>
-          <div style={{position: 'absolute', right:'5%' }}>
-          <a href="#" className="item" style={{marginRight:'20px'}}>Facebook</a>
-          <a href="#" className="item" style={{marginRight:'20px'}}>Twitter</a>
-          <a href="#" className="item">Github</a>
+          <div className="right-footer">
+          <a href="#" className="right-footer-item">Facebook</a>
+          <a href="#" className="right-footer-item">Twitter</a>
+          <a href="#" className="right-footer-item">Github</a>
         </div>
       </div>
     </div>
